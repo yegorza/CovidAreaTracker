a5b950adc887a55319eb9bad90d0a5c939d67573
yegorza <yegorza@gmail.com>
2021-05-22 13:39:03 -0400

    added health unit ids, made final output show time

--- covidAreaTracker.py
+++ covidAreaTracker.py
@@ -12,6 +12,7 @@ from ipyleaflet import Map, AntPath, MeasureControl
 import ipywidgets
 from shapely.geometry import Point
 import healthUnitCoordinateStrings
+import datetime
 
 def getPolygonFromStr(geocodesString):
     coordArray = []
@@ -24,6 +25,40 @@ def getPolygonFromStr(geocodesString):
     
     
     return Polygon([coordArray])
+def convertToReadableDate(dateValue):
+    convertedDate = datetime.datetime.strptime(dateValue, '%Y-%m-%dT%H:%M:%S')
+    readableDate = datetime.datetime.strftime(convertedDate,'%B %d, %Y')
+    return readableDate
+
+def getHealthUnitStatusFromAPI(healthUnitId):
+    userLimit = 500
+    # userLocation = input("Enter your area's id (within your limit)")
+    url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=ce9f043d-f0d4-40f0-9b96-4c8a83ded3f6&limit='+ str(userLimit)  
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        responseData = response.read()
+        responseStr = str(responseData, 'utf-8')
+        responseJson = json.loads(responseStr)
+        #print("Response: ", responseJson)
+        print("=====================================")
+        records = responseJson['result']['records']
+
+    lastStatusRecord = records[len(records)]
+
+    print("======HISTORY OF STATUSES======")
+    for record in records:
+       #print("Record: ", record)
+       #Print out the health centre status based on user's location
+       if(record['Reporting_PHU_id'] == healthUnitId):
+           print("In your current location these are the requirements: ")
+           print("Health Unit Id: ", record['Reporting_PHU_id'], "Id: ", record['_id'],"Health Unit:", record['Reporting_PHU'])
+           print("Status: ", record['Status_PHU'])
+           print("Start Date: ",convertToReadableDate(record['start_date']))
+           print("End Date: ",convertToReadableDate(record['end_date']))
+           print("===========================")
+       #else:
+           #print("SKIPPING: Id: ", record['_id'],"Health Unit:", record['Reporting_PHU'],"Status: ", record['Status_PHU'],"Health Unit Id: ", record['Reporting_PHU_id']) 
+
 
 #Get User Point
 userPoint = geocoder.ip('')
@@ -65,8 +100,20 @@ healthUnits = [waterlooHealthRegionPolygon,yorkRegionPolygon,huronPerthPolygon,s
 leedsGrenvilleLanarkDistrictHealthPolygon, haldimandNorfolkHealthPolygon, timiskamingHealthPolygon, renfrewCountyDistrictHealthPolygon,torontoPublicHealthPolygon]
 
 #Initialize Array of Healt Unit Names
-healthUnitNames = ["Waterloo Health Unit","York Region Health Unit","Huron Perth Healht Unit","Southwestern Health Unit","Porcupine Health Unit","Hamilton Public Health Unit","Thunder Bay Health Unit","Peel Health Unit","Lambton Health Unit","Wellington Public Health Unit","Brant County Health Unit","Middle-Sex London Health Unit","Sudbury and District Health Unit","Niagra Public Health Unit","ChathamKent Health Unit","Kingston, Frontenac, Lennox and Addington Health Unit","Windsor Essex County Public Health Unit","Peterbrough Health Unit","Grey Bruce Health Unit","Eastern Ontario Health Unit","North Bay Parry Sound Health Unit","Ottawa Public Health Unit","Leeds, Grenville and Lanark Health Unit","Haldimand Norfolk Health Unit","Timiskaming Health Unit","Renfrew County District Health Unit","Toronto Health Unit"]
+healthUnitNames = ["Waterloo Health Unit","York Region Health Unit","Huron Perth Healht Unit","Southwestern Health Unit","Porcupine Health Unit",
+"Hamilton Public Health Unit","Thunder Bay Health Unit","Peel Health Unit","Lambton Health Unit","Wellington Public Health Unit",
+"Brant County Health Unit","Middle-Sex London Health Unit","Sudbury and District Health Unit","Niagra Public Health Unit","ChathamKent Health Unit",
+"Kingston, Frontenac, Lennox and Addington Health Unit","Windsor Essex County Public Health Unit","Peterbrough Health Unit","Grey Bruce Health Unit","Eastern Ontario Health Unit",
+"North Bay Parry Sound Health Unit","Ottawa Public Health Unit","Leeds, Grenville and Lanark Health Unit","Haldimand Norfolk Health Unit","Timiskaming Health Unit",
+"Renfrew County District Health Unit","Toronto Health Unit"]
 
+#Initialize Health Unit Id's
+healthUnitIds = [2265,2270,5183,4913,2256,
+2237,2262,2253,2242,2266,
+2227,2244,2261,2246,2240,
+2241,2268,2255,2233,2258,
+2247,2251,2243,2234,2263,
+2257,3895]
 #Goes through all health units and checks which one user is located in
 i = 0
 for unit in healthUnits:
@@ -76,9 +123,11 @@ for unit in healthUnits:
         print("=========================================")
         print("You are Located in: ", healthUnitNames[i])
         print("=========================================")
+        getHealthUnitStatusFromAPI(healthUnitIds[i])
     elif boolean_point_in_polygon(userPointCheck, unit) == False:
         print("You are Not Located in: ", healthUnitNames[i])
 
+    
     #Checks all health unit however prints out coords    
     #print(healthUnitNames[i],"You are located in:",boolean_point_in_polygon(userPointCheck,unit))
     i+=1
